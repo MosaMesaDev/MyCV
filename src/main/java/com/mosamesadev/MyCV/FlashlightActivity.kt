09b8c9446d3070969694ce8c9014aee83a6ffffd
Hendrik Brouwer <h.brouwer@gmail.com>
2021-12-16 13:49:31 +0100

    Redesigning Rotate activity (not complete yet)

--- src/main/java/com/mosamesadev/MyCV/FlashlightActivity.kt
+++ src/main/java/com/mosamesadev/MyCV/FlashlightActivity.kt
@@ -7,13 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 
 class FlashlightActivity : AppCompatActivity() {
     private lateinit var cameraM :CameraManager
     private lateinit var powerBtn:ImageButton
-    var isFlash = false
+    private var isFlash = false
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,20 +33,13 @@ class FlashlightActivity : AppCompatActivity() {
             cameraM.setTorchMode(cameraListId,true)
             isFlash = true
             powerBtn.setImageResource(R.drawable.ic_power_on)
-            textMessage("Flash Light is On",this)
         }
         else{
             val cameraListId = cameraM.cameraIdList[0]
             cameraM.setTorchMode(cameraListId,false)
             isFlash = false
             powerBtn.setImageResource(R.drawable.ic_power_off)
-            textMessage("Flash Light is Off",this)
         }
-
-    }
-
-    private fun textMessage(s: String, c:Context) {
-        Toast.makeText(c,s,Toast.LENGTH_SHORT).show()
 
     }
 
